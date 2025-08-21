bba803baf32b431569316a1ee2609ddcd7d0b26e
LEANDR0C <leandrocampos11111@gmail.com>
2025-08-21 01:57:04 -0300

    funcion suma

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 function name(params) {
-    
+    x = 0;
+    console.log(x + 5);
 }
