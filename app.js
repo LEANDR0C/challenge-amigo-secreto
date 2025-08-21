45f99df97ac74577fe97cc05893e024d45b3d858
LEANDR0C <leandrocampos11111@gmail.com>
2025-08-21 01:53:17 -0300

    console

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
 function name(params) {
-    
+    console.log("prueba");
 }
